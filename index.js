dbfe5d64e710c6cf1fc970a3e61bf3abb4ae3f84
github1104 <vipboytk98@gmail.com>
2020-03-27 00:07:44 +0700

    test ok

--- index.js
+++ index.js
@@ -22,46 +22,44 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
-    try {
-        if (!message.guild) return;
-        if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        const cmd = args.shift().toLowerCase();
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-        if (cmd === "ping") {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd === "ping") {
+        try {
             const msg = await message.channel.send(' Pinging...');
 
             msg.edit(`Pong!\nPing is ${Math.floor(msg.createdAt - message.createdAt)}ms`);
+        } catch (error) {
+            console.log(error)
         }
+    }
 
-        if (cmd === "say") {
+    if (cmd === "say") {
 
-            if (!args.length)
-                return message.reply("Nothing to say?").then(m => m.delete(5000));
-            const roleColor = message.guild.me.displayHexColor === "#000000" ? "#ffffff" : message.guild.me.displayHexColor;
+        if (!args.length)
+            return message.reply("Nothing to say?").then(m => m.delete(5000));
+        const roleColor = message.guild.me.displayHexColor === "#000000" ? "#ffffff" : message.guild.me.displayHexColor;
 
-            if (args[0].toLowerCase() === "embed") {
-                const embed = new MessageEmbed()
-                    .setColor(roleColor)
-                    .setDescription(args.slice(1).join(" "))
-                    .setTimestamp()
-                    .setImage(client.user.displayAvatarURL())
-                    .setAuthor(message.author.username, message.author.displayAvatarURL())
-                    .setFooter(client.user.username, client.user.displayAvatarURL())
-                message.channel.send(embed);
-            }
-
-        }
-
-        if (cmd === "server") {
-            message.channel.send(`This server is : ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new MessageEmbed()
+                .setColor(roleColor)
+                .setDescription(args.slice(1).join(" "))
+                .setTimestamp()
+                .setImage(client.user.displayAvatarURL())
+                .setAuthor(message.author.username, message.author.displayAvatarURL())
+                .setFooter(client.user.username, client.user.displayAvatarURL())
+            message.channel.send(embed);
         }
 
     }
-    catch(e){
-        console.log(e)
-        return null
+
+    if (cmd === "server") {
+        message.channel.send(`This server is : ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     }
 
 });
